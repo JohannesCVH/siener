63e46e30a31d62c3f62b24cda587c8fc642ff750
Johannes van Heerden <johannescvh@icloud.com>
2026-03-07 09:31:33 +0200

    Saving push notification now also updates existing for user.

--- src/api/SpiEyes/Controllers/PushNotificationController.cs
+++ src/api/SpiEyes/Controllers/PushNotificationController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SpiEyes.DAL;
 using SpiEyes.Models;
 
@@ -16,12 +17,24 @@ public class PushNotificationController : ControllerBase
     }
 
     [HttpPost("SaveSubscription")]
-    public async Task<IActionResult> SaveSubscription([FromBody] PushSubscription pushSubscription)
+    public async Task<IActionResult> SaveSubscription([FromBody] Models.PushSubscription pushSubscription)
     {
         try
         {
+            var existing = await _databaseContext.PushSubscriptions.Include(x => x.Keys).FirstOrDefaultAsync(x => x.UserID == pushSubscription.UserID);
+            if (existing != null)
+            {
+                existing.Endpoint = pushSubscription.Endpoint;
+                existing.Keys.P256dh = pushSubscription.Keys.P256dh;
+                existing.Keys.Auth = pushSubscription.Keys.Auth;
+                _databaseContext.PushSubscriptions.Update(existing);
+            }
+            else
+        {
             pushSubscription.Id = Guid.NewGuid();
             await _databaseContext.AddAsync(pushSubscription);
+            }
+            
             await _databaseContext.SaveChangesAsync();
             return new CreatedResult();
         }
