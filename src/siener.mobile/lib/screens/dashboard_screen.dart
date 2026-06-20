10d3695893687afcfd25e96a9f25622a3efb42c0
Johannes van Heerden <johannesvheerden@gmail.com>
2026-06-20 11:00:36 +0200

    Added camera and dashboard screens to client

--- src/siener.mobile/lib/screens/dashboard_screen.dart
+++ src/siener.mobile/lib/screens/dashboard_screen.dart
@@ -1,76 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:siener.mobile/logger.dart';
 import 'package:siener.mobile/models/camera.dart';
+import 'package:siener.mobile/screens/camera_screen.dart';
 import 'package:siener.mobile/services/camera_service.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
-
+  
   @override
   State<DashboardScreen> createState() => _DashboardScreenState();
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+
   final CameraService _cameraService = CameraService();
-  late Future<List<Camera>> _camerasFuture;
+  List<Camera>? _cameras;
+  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    _camerasFuture = _cameraService.fetchCameras();
+    _loadCameras();
   }
 
+  Future<void> _loadCameras() async {
+    const String functionName = '_loadCameras';
+    
+    _isLoading = true;
+    final List<Camera>? cameras = await _cameraService.fetchCameras();
+    
+    if (cameras != null) {
+      setState(() {
+        _cameras = cameras;
+        _cameras?.forEach((camera) => logMessage(_DashboardScreenState, functionName, 'Camera loaded: ${camera.name}'));
+        _isLoading = false;
+      });
+    } else {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Dashboard")),
-      body: FutureBuilder<List<Camera>>(
-        future: _camerasFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('No cameras found'));
-          }
-
-          final cameras = snapshot.data!;
-          return GridView.builder(
-            padding: const EdgeInsets.all(8),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, // 2 columns
-              childAspectRatio: 1.0,
-              crossAxisSpacing: 8,
-              mainAxisSpacing: 8,
-            ),
-            itemCount: cameras.length,
-            itemBuilder: (context, index) {
-              final camera = cameras[index];
-              return InkWell(
-                onTap: () {
-                  // TODO: Navigate to CameraPlayerScreen
-                  print("Clicked: ${camera.name}");
-                },
-                child: Card(
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: Image.network(
-                          _cameraService.getThumbnailUrl(camera.name),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(camera.name),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          );
-        },
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const CameraScreen()),
+            );
+          }, child: const Text('Camera'),
+        ),
       ),
     );
   }
