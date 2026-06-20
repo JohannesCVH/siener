e43c7a5207489df9b103c0892b1e9834f2387b8e
Johannes van Heerden <johannesvheerden@gmail.com>
2026-06-20 10:59:06 +0200

    Added CameraService to siener.mobile

--- src/siener.mobile/lib/services/camera_service.dart
+++ src/siener.mobile/lib/services/camera_service.dart
@@ -1,27 +1,36 @@
 import 'dart:convert';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:siener.mobile/logger.dart';
 import 'package:siener.mobile/models/camera.dart';
 import 'package:siener.mobile/http_client.dart';
 
 class CameraService {
   final String _baseUrl;
+  final int _basePort;
 
-  CameraService(): _baseUrl = dotenv.get('API_BASE_URL');
+  CameraService(): 
+    _baseUrl = dotenv.get('API_BASE_URL'), 
+    _basePort = int.parse(dotenv.get('API_BASE_PORT'));
 
-  Future<List<Camera>> fetchCameras() async {
+  Future<List<Camera>?> fetchCameras() async {
     final client = await getHttpClientWithCert();
-    final response = await client.get(Uri.parse('$_baseUrl/api/Camera/Streams'));
 
-    if (response.statusCode == 200) {
-      final List<dynamic> body = jsonDecode(response.body);
-      return body.map((dynamic item) => Camera.fromJson(item)).toList();
-    } else {
-      throw Exception('Failed to load cameras');
+    try {
+      final response = await client.get(Uri.parse('$_baseUrl:$_basePort/api/Camera/Streams'));
+      
+      if (response.statusCode == 200) {
+        final List<dynamic> body = jsonDecode(response.body);
+        return body.map((dynamic item) => Camera.fromJson(item)).toList();
+      }
+    } catch (e) {
+      logError(CameraService, 'fetchCameras', 'Failed to load cameras: $e');
     }
+
+    return null;
   }
 
   String getThumbnailUrl(String cameraName) {
-    return '$_baseUrl/api/Camera/GetThumbnail/$cameraName';
+    return '$_baseUrl:$_basePort/api/Camera/GetThumbnail/$cameraName';
   }
 }
