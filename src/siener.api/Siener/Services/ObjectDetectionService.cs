4be8dd55f6134b1b5da0a7dc8d1caf3693afff64
Johannes van Heerden <johannesvheerden@gmail.com>
2026-09-21 11:36:32 +0200

    Added logging to ObjectDetectionsService

--- src/siener.api/Siener/Services/ObjectDetectionService.cs
+++ src/siener.api/Siener/Services/ObjectDetectionService.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics;
 using Siener.Models;
+using Siener.Utility;
 using SkiaSharp;
 using YoloDotNet;
 
@@ -6,22 +8,32 @@ namespace Siener.Services;
 
 public interface IObjectDetectionService
 {
-    public Task<IEnumerable<ObjectDetectionResponse>> DetectAsync(byte[] buffer);
+    public Task<IEnumerable<ObjectDetectionResponse>> DetectAsync(string camera, byte[] buffer);
 }
 
 public class ObjectDetectionService : IObjectDetectionService
 {
     private readonly Yolo _yoloEngine;
+    private readonly ILogger<ObjectDetectionService> _logger;
     
-    public ObjectDetectionService(Yolo yoloEngine)
+    public ObjectDetectionService(
+        Yolo yoloEngine,
+        ILogger<ObjectDetectionService> logger
+    )
     {
         _yoloEngine = yoloEngine;
+        _logger = logger;
     }
 
-    public async Task<IEnumerable<ObjectDetectionResponse>> DetectAsync(byte[] buffer)
+    public async Task<IEnumerable<ObjectDetectionResponse>> DetectAsync(string camera, byte[] buffer)
     {
+        string methodName = nameof(DetectAsync);
+        
         try
         {
+            var sw = new Stopwatch();
+            sw.Start();
+            
             using var bitmap = SKBitmap.Decode(buffer);
             if (bitmap == null) throw new ArgumentException("Invalid image stream.");
             var results = _yoloEngine.RunObjectDetection(bitmap).Select(r => new ObjectDetectionResponse{
@@ -29,11 +41,26 @@ public class ObjectDetectionService : IObjectDetectionService
                 Confidence = r.Confidence
             });
 
+            _logger.LogMessage(LogType.Information, methodName, $"Object detection service responded in: {sw.ElapsedMilliseconds}ms for camera: {camera}");
+
+            _logger.LogMessage(LogType.Information, methodName, $"[Objects Detected] -> Amount: {results.Count()}", new Dictionary<string, string>() { { "Camera", camera } });
+            foreach (var detection in results)
+            {
+                if (detection.Label == "person" || detection.Label == "dog")
+                {
+                    _logger.LogMessage(LogType.Information, methodName, $"[Object detected] -> Label: {detection.Label}, Confidence: {detection.Confidence}", new Dictionary<string, string>() { 
+                        { "Camera", camera },
+                        { "Label", detection.Label },
+                        { "Confidence", detection.Confidence.ToString() }
+                    });
+                }
+            }
+
             return results;
         }
         catch(Exception ex)
         {
-            Console.WriteLine($"[ObjectDetectionService -> DetectAsync] | ERROR: {ex.Message}");
+            Console.WriteLine($"ERROR: {ex.Message}");
         }
         
         return [];
